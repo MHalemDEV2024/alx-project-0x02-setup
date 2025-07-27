c423a6bae23f5256e55ae3ae0a49c714cca611e6
Mohamed Abdelhlim Saeed <MHalem_DEV@outlook.com>
2025-07-27 16:08:01 +0300

    Implement Navigation and Layout

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,18 +2,15 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-white shadow p-4 flex justify-between items-center">
-      <h2 className="text-2xl font-semibold text-gray-800">ALX Project</h2>
-      <nav className="space-x-4">
-        <Link href="/" className="text-blue-600 hover:underline">
-          Home
-        </Link>
-        <Link href="/home" className="text-blue-600 hover:underline">
-          /home
-        </Link>
-        <Link href="/about" className="text-blue-600 hover:underline">
-          /about
-        </Link>
+    <header className="bg-white shadow p-4 flex justify-center space-x-8">
+      <nav>
+        <Link href="/home" className="text-blue-600 hover:underline">Home</Link>
+      </nav>
+      <nav>
+        <Link href="/about" className="text-blue-600 hover:underline">About</Link>
+      </nav>
+      <nav>
+        <Link href="/posts" className="text-blue-600 hover:underline">Posts</Link>
       </nav>
     </header>
   );
